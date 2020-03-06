01b2946b1f403661b7571740a7ce8cf2f6cb5966
Brian Olore <brian@olore.net>
2020-03-06 08:12:48 -0500

    add studyguide

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Game from './Game';
+import StudyGuide from './StudyGuide';
 
 function App() {
+  const [playGame, setPlayGame] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Let's Meet the Team!</h1>
       </header>
-      <Game></Game>
+
+      {playGame &&
+        <div>
+          <Game></Game>
+          <div style={{marginTop: '30px'}}>
+            <button style={{fontSize: '20px'}} onClick={() => { setPlayGame(false);}}>Study Guide</button>
+          </div>
+        </div>
+      }
+      {!playGame &&
+        <div>
+          <button onClick={() => { setPlayGame(true);}}>Back to Game</button>
+          <h2>But first, let's study!</h2>
+          <StudyGuide></StudyGuide>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/StudyGuide.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import data from './data.json';
+import * as sortby from 'lodash.sortby';
+
+export default function StudyGuide(props) {
+
+  const getPeopleCards = () => {
+    const sortedPeople = sortby(data.people, ['lastName', 'firstName']);
+
+    return sortedPeople.map((person, idx) => {
+      let name = person.firstName + ' ' + person.lastName;
+      return <div style={{padding: "10px", maxWidth: "180px"}} key={idx}>
+        <img alt={name} height="150" src={person.image}></img>
+        <div>{person.firstName + ' ' + person.lastName}</div>
+        <div>{person.position}</div>
+        <div>{person.city}</div>
+      </div>;
+    });
+  } 
+
+  return <div style={{
+      display: 'flex', 
+      flexDirection: 'row', 
+      flexWrap: 'wrap',
+      margin: '0 100px',
+      }}>
+    {getPeopleCards()}
+  </div>;
+}
